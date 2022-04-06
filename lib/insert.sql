3c144d8ae1b6c2661be8ad933cf988358252e2ae
Christopher Terrano <mterrano1@yahoo.com>
2022-04-06 14:12:55 -0500

    commit tree 77de1399f256790a094823b7853bb93550225c74

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Grumpy', 0);
+INSERT INTO bears VALUES (2, 'Rowdy', 7, 'M', 'Brown', 'Happy', 0);
+INSERT INTO bears VALUES (3, 'Tabitha', 2, 'F', 'Black', 'Loving', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 8, 'M', 'Brown', 'Territorial', 0);
+INSERT INTO bears VALUES (5, 'Melissa', 2, 'F', 'Black', 'Playfull', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 9, 'M', 'Black', 'Angry', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 7, 'F', 'Brown', 'Hungry', 0);
+INSERT INTO bears VALUES (8, NULL, 6, 'M', 'Brown', 'Mysterious', 1);
+
+-- INSERT INTO bears VALUES 
+--     ('Mr. Chocolate', 5, 'M', 'Brown', 'Grumpy', 0),
+--     ('Rowdy', 7, 'M', 'Brown', 'Happy', 0),
+--     ('Tabitha', 2, 'F', 'Black', 'Loving', 1),
+--     ('Sergeant Brown', 8, 'M', 'Brown', 'Territorial', 0),
+--     ('Melissa', 2, 'F', 'Black', 'Playfull', 1),
+--     ('Grinch', 9, 'M', 'Black', 'Angry', 1),
+--     ('Wendy', 7, 'F', 'Brown', 'Hungry', 0),
+--     (NULL, 6, 'M', 'Brown', 'Mysterious', 1);
